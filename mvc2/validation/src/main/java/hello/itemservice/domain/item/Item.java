81e48766344f5aa6b55f65528be39cab0c7a347c
Hojun <hojun.cha997@gmail.com>
2022-01-11 23:48:41 +0900

    The limitations of bean validation

--- mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,6 +12,12 @@ import javax.validation.constraints.NotNull;  // 어떤 구현체에서도 동�
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원이 넘도록 입력해주세요") //오브젝트 자체의 (필드오류가 아닌)) 오류를 처리. 그러나 실무에서 사용하기엔 기능이 부족하다
 public class Item {
 
+    // Bean validation의 한계
+
+    // 만약 수정 요구사항에서 id값이 반드시 필요하고, 수량도 무제한으로 하는 정책이 생겼다고 가정하자.
+    // id필드에 @NotNull을 붙이고, @Max를 없애버리면 어떻게 될까? 이미 등록된 게시글은 수정할 수 있겠지만 새로운 등록 시 문제가 발생한다.
+    // id값이 없어서 오류가 발생하고, 수량은 무제한으로 등록이 가능하다. 따라서 어노테이션만으로 모든 문제를 해결할 수 는 없다.
+
     private Long id;
 
     @NotBlank
--- mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.naming.Binding;
 import java.util.List;
 
 @Slf4j
@@ -32,21 +33,21 @@ public class ValidationItemControllerV3 {
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "validation/v2/items";
+        return "validation/v3/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v2/item";
+        return "validation/v3/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
         //새로운 객체르 만들어서 넣어준 이유는 여러가지이지만, 검증에 실패했을 때 그대로 파라미터 값을 사용해서 돌려주기 위한 이유도 있다.
-        return "validation/v2/addForm";
+        return "validation/v3/addForm";
     }
 
 
@@ -73,14 +74,14 @@ public class ValidationItemControllerV3 {
         // 글로벌 검증을 하려면 주석처리. ItemServiceApplication에 코드 추가
         if(bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
-            return "validation/v2/addform";
+            return "validation/v3/addform";
         }
         
 
         //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
             log.info("errors={} ", bindingResult);
-            return "validation/v2/addForm";
+            return "validation/v3/addForm";
         }
         //이렇게 하니까 컨트롤러가 하는 역할이 너무 많아졌다. 실제 성공로직에 비해 검증로직이 더욱 많다.
         // 따라서 클래스를 생성해서 거기에 validation을 하는 로직을 넣어줌으로써 이 컨트롤러에서 분리해 낼 것이다.
@@ -107,11 +108,26 @@ public class ValidationItemControllerV3 {
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v2/editForm";
+        return "validation/v3/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+        //수정 내용에 대한 검증도 @Validated로 검증하면 된다. BindingResult 삽입도 잊지 말 것.
+
+        //특정 필드가 아닌 복합 룰 검증. 도메인에서 @ScriptAssert()로 처리해 줄 수도 있지만 복잡한 경우를 처리하기엔 기능이 부족하다(다른 객체를 조회할 필요가 있을 수도 있고) 그래서 차라리 자바코드로 처리한다.
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
